2d875aa845d3f7f8bba4bfc76d0cbe8f3f8a4909
theabbie <abhishek7gg7@gmail.com>
2022-04-13 13:35:38 +0530

    Leetcode submissions

--- 01-matrix.py
+++ 01-matrix.py
@@ -1,10 +1,11 @@
 import heapq
+from collections import defaultdict
 
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         dist = [[0 if cell == 0 else float('inf') for cell in row] for row in mat]
         heap = []
-        deleted = {}
+        deleted = defaultdict(int)
         m = len(mat)
         n = len(mat[0])
         for i in range(m):
@@ -17,20 +18,18 @@ class Solution:
                 if deleted[(currdist, i, j)] == 0:
                     del deleted[(currdist, i, j)]
                 currdist, i, j = heapq.heappop(heap)
-            if i > 0 and dist[i - 1][j] > 1 + currdist:
-                deleted[(dist[i - 1][j], i - 1, j)] = deleted.get((dist[i - 1][j], i - 1, j), 0) + 1
-                dist[i - 1][j] = 1 + currdist
-                heapq.heappush(heap, (1 + currdist, i - 1, j))
-            if i < m - 1 and dist[i + 1][j] > 1 + currdist:
-                deleted[(dist[i + 1][j], i + 1, j)] = deleted.get((dist[i + 1][j], i + 1, j), 0) + 1
-                dist[i + 1][j] = 1 + currdist
-                heapq.heappush(heap, (1 + currdist, i + 1, j))
-            if j > 0 and dist[i][j - 1] > 1 + currdist:
-                deleted[(dist[i][j - 1], i, j - 1)] = deleted.get((dist[i][j - 1], i, j - 1), 0) + 1
-                dist[i][j - 1] = 1 + currdist
-                heapq.heappush(heap, (1 + currdist, i, j - 1))
-            if j < n - 1 and dist[i][j + 1] > 1 + currdist:
-                deleted[(dist[i][j + 1], i, j + 1)] = deleted.get((dist[i][j + 1], i, j + 1), 0) + 1
-                dist[i][j + 1] = 1 + currdist
-                heapq.heappush(heap, (1 + currdist, i, j + 1))
+            adj = []
+            if i > 0:
+                adj.append((i - 1, j))
+            if i < m - 1:
+                adj.append((i + 1, j))
+            if j > 0:
+                adj.append((i, j - 1))
+            if j < n - 1:
+                adj.append((i, j + 1))
+            for a, b in adj:
+                if dist[a][b] > 1 + currdist:
+                    deleted[(dist[a][b], a, b)] += 1
+                    dist[a][b] = 1 + currdist
+                    heapq.heappush(heap, (1 + currdist, a, b))
         return dist
